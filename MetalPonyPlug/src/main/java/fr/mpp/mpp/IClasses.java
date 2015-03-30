2747d04bd85fc775ac5035f4d94bc9e50da6a18c
JesFot <jesfot@gmail.com>
2015-03-30 22:42:10 +0200

    Update IClasses.java

--- MetalPonyPlug/src/main/java/fr/mpp/mpp/IClasses.java
+++ MetalPonyPlug/src/main/java/fr/mpp/mpp/IClasses.java
@@ -1,6 +1,17 @@
 package fr.mpp.mpp;
 
+/*
+ * Interface for all Ranks
+ */
 public interface IClasses
 {
-	// Code ...
+	public String getName();
+	public void setName(String name);
+
+	public String getReason();
+	public void setReason(String reason);
+
+	public List<Player> getPlayersConcern();
+	public void setPlayerConcern(Player player);
+	public void removeConcernPlayer(Player player);
 }
