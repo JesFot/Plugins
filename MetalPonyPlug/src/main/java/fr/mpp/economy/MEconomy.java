2316bc488872d63ef40f083bbca800db7fa733a3
JesFot <jesfot@gmail.com>
2015-05-02 12:17:40 +0200

    Add MPEco

--- MetalPonyPlug/src/main/java/fr/mpp/economy/MEconomy.java
+++ MetalPonyPlug/src/main/java/fr/mpp/economy/MEconomy.java
@@ -1,6 +1,10 @@
 package fr.mpp.economy;
 
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+
 public class MEconomy
 {
-	//
+	protected Map<Player, MPEconomy> eco;
 }
---
+++ MetalPonyPlug/src/main/java/fr/mpp/economy/MPEconomy.java
@@ -0,0 +1,38 @@
+package fr.mpp.economy;
+
+import org.bukkit.entity.Player;
+
+import fr.mpp.MetalPonyPlug;
+import fr.mpp.config.MConfig;
+
+public class MPEconomy
+{
+	private MetalPonyPlug mpp;
+	private Player player;
+	private MConfig config;
+	
+	public MPEconomy(MetalPonyPlug p_mpp, Player p_player)
+	{
+		this.mpp = p_mpp;
+		this.player = p_player;
+		this.config = this.mpp.getConfig();
+	}
+	
+	public Double getMoney()
+	{
+		this.config.reloadCustomConfig();
+		return this.config.getCustomConfig().getDouble("eco.money."+player.getName().toLowerCase(), 0);
+	}
+	
+	public void setMoney(Double p_money)
+	{
+		this.config.getCustomConfig().set("eco.money."+player.getName().toLowerCase(), p_money);
+		this.config.saveCustomConfig();
+	}
+	
+	public void resetMoney()
+	{
+		Double baseM = this.config.getCustomConfig().getDouble("eco.basemoney", 0);
+		this.config.getCustomConfig().set("eco.money."+player.getName().toLowerCase(), baseM);
+	}
+}
