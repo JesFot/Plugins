ac6d561ec6f0ae842eff023011a248211a112ec9
JesFot <jesfot@gmail.com>
2015-09-18 19:42:54 +0200

    Update and rename TestmppCommand.java to TestgbpCommand.java

--- MetalPonyPlug/src/main/java/fr/mpp/command/TestgbpCommand.java
+++ MetalPonyPlug/src/main/java/fr/mpp/command/TestgbpCommand.java
@@ -1,15 +1,15 @@
-package fr.mpp.command;
+package fr.gbp.command;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class TestmppCommand implements CommandExecutor
+public class TestgbpCommand implements CommandExecutor
 {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
-		if (label.equalsIgnoreCase("testmpp"))
+		if (label.equalsIgnoreCase("testgbp"))
 		{
 			String msg;
 			if (args.length >= 1)
