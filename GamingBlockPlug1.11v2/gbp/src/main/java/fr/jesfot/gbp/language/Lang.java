a6b74b8aadcc255cd67691f9b5e80a50578005a8
JesFot <jesfot@gmail.com>
2017-05-30 16:25:22 +0200

    Copied lang system

---
+++ GamingBlockPlug1.11v2/gbp/src/main/java/fr/jesfot/gbp/language/Lang.java
@@ -0,0 +1,65 @@
+package fr.jesfot.gbp.language;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lang
+{
+	private static Map<Integer, Lang> BY_ID = new HashMap<Integer, Lang>();
+	private static Map<String, Lang> BY_NAME = new HashMap<String, Lang>();
+	
+	public static Lang DEFAULT = new Lang(-1, "default-lang", "en_US.lang", "en_US");
+	public static Lang en_US = new Lang(0, "english_USA", "en_US.lang", "en_US");
+	public static Lang fr_FR = new Lang(1, "francais", "fr_FR.lang", "fr_FR");
+
+	protected int id;
+	protected String name;
+	protected String genericLang;
+	protected String file;
+
+	public Lang(int p_id, String p_name, String p_file, String pointer)
+	{
+		this.name = p_name;
+		this.file = p_file;
+		this.genericLang = pointer;
+		this.id = p_id;
+		BY_ID.put(new Integer(this.id), this);
+		BY_NAME.put(pointer, this);
+	}
+	
+	public String getFile()
+	{
+		return this.file;
+	}
+	
+	public int getID()
+	{
+		return this.id;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return this.genericLang;
+	}
+	
+	public static Lang getByID(int id)
+	{
+		Lang result = BY_ID.get(new Integer(id));
+		if(result == null)
+		{
+			return DEFAULT;
+		}
+		return result;
+	}
+	
+	public static Lang getByName(String name)
+	{
+		Lang result = BY_NAME.get(name);
+		if(result == null)
+		{
+			return DEFAULT;
+		}
+		return result;
+	}
+}
