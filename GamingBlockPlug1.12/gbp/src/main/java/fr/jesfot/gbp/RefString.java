48083bc78184fb47ec44a6b038e57dc6f734aaeb
JesFot <jesfot@gmail.com>
2017-07-13 12:55:40 +0200

    Update mc version in strings

--- GamingBlockPlug1.12/gbp/src/main/java/fr/jesfot/gbp/RefString.java
+++ GamingBlockPlug1.12/gbp/src/main/java/fr/jesfot/gbp/RefString.java
@@ -3,7 +3,7 @@ package fr.jesfot.gbp;
 public class RefString 
 {
     public static final String NAME = "GamingBlockPlug";
-    public static final String MCVERSION = "1.11";
+    public static final String MCVERSION = "1.12";
     public static final Boolean OPOVERRIDE = true;
     
     public static final String BOT_NAME = "GamingBlockBot";
